f16bf483f825ad619d055b56386380bc86893bc2
naylakhansaazilla <naylakhansaakaderisasi@gmail.com>
2026-06-05 15:58:50 +0700

    Update fungsi main.py

--- Tugas-09/main.py
+++ Tugas-09/main.py
@@ -12,25 +12,24 @@
 from tools import display_menu, select_menu
 
 if __name__ == '__main__':
-
-    print('======================================')
-print('      Selamat Datang di Cheki-Cheki ^^')
-print('======================================')
+    
+    print('================================')
+    print('Selamat datang di Cheki-Cheki ^^')
+    print('================================')
 
 nama = input('Silakan masukkan namamu ya!')
 
 print()
-print(f'Baik, {user}! Selamat mengatur jadwal harianmu di Cheki-Cheki ya!')
+print(f'Baik, {nama}! Selamat mengatur jadwal harianmu di Cheki-Cheki ya!')
 print('Yuk, susun kegiatanmu agar lebih teratur dan produktif!')
 print()
 
-    while True:
-        display_menu()
-
-        menu = input('Masukkan menu yang dipilih: ')
-
-        is_done = select_menu(menu=menu)
-
-        if is_done:
-            print(f'Sampai jumpa, {nama}!')
-            break
+while True:
+    display_menu()
+    
+    menu = input('Masukkan menu yang dipilih: ')
+    
+    is_done = select_menu(menu)
+    
+    if is_done:
+        break
