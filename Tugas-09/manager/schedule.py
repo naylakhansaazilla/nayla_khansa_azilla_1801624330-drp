61f919cd94cedf6a89f882de29fa7cf73d12d7e6
nalaputriandini23 <nalaputriandini23@gmail.com>
2026-06-05 14:41:51 +0700

    menambahkan fungsi schedule.py

---
+++ Tugas-09/manager/schedule.py
@@ -0,0 +1,18 @@
+def add_schedule():
+    print('Menambahkan jadwal baru...')
+
+
+def show_schedule():
+    print('Menampilkan daftar kegiatan...')
+
+
+def checklist_schedule():
+    print('Menandai kegiatan sebagai selesai...')
+
+
+def edit_schedule():
+    print('Mengubah jadwal...')
+
+
+def delete_schedule():
+    print('Menghapus jadwal...')
