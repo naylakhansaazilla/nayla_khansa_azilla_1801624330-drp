0edc99f1ed31bbe64b02506f34546a7aef574d5e
Tasya Aulia Ramadhani <ramadhanitasyaulia@gmail.com>
2026-06-05 14:37:38 +0700

    menambahkan fungsi reminder.py

---
+++ Tugas-09/manager/reminder.py
@@ -0,0 +1,5 @@
+def check_reminder():
+    print('================================')
+    print('       PENGINGAT JADWAL')
+    print('================================')
+    print('Mengecek jadwal yang akan datang...')
