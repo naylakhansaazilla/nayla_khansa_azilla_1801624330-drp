8fd18a41fb2a2ed13b08e0464d640af423e6dd73
naylakhansaazilla <naylakhansaakaderisasi@gmail.com>
2026-06-19 14:04:52 +0700

    Update main.py

--- Tugas-09/main.py
+++ Tugas-09/main.py
@@ -1,40 +1,29 @@
-# Penyimpanan data
-
-# Komponen aplikasi perangkat lunak:
-# 1. Create (Tambah Jadwal)
-# 2. Read (Lihat Jadwal)
-# 3. Update (Edit Jadwal / Checklist)
-# 4. Delete (Hapus Jadwal)
-
-# Aplikasi: Cheki-Cheki
-# Aplikasi pengelola jadwal harian
-
 from tools import display_menu, select_menu
-from manager.storage import create_table
+from manager.storage import create_table, get_or_create_user
 
 if __name__ == '__main__':
 
     create_table()
-    
+
     print('================================')
     print('Selamat datang di Cheki-Cheki ^^')
     print('================================')
 
-    nama = input('Silakan masukkan namamu ya!')
-    
+    nama = input('Silakan masukkan namamu ya! ')
+
     id_user = get_or_create_user(nama)
 
-print()
-print(f'Baik, {nama}! Selamat mengatur jadwal harianmu di Cheki-Cheki ya!')
-print('Yuk, susun kegiatanmu agar lebih teratur dan produktif!')
-print()
+    print()
+    print(f'Baik, {nama}! Selamat mengatur jadwal harianmu di Cheki-Cheki ya!')
+    print('Yuk, susun kegiatanmu agar lebih teratur dan produktif!')
+    print()
 
-while True:
-    display_menu()
-    
-    menu = input('Masukkan menu yang dipilih: ')
-    
-    is_done = select_menu(menu)
-    
-    if is_done:
-        break
+    while True:
+        display_menu()
+
+        menu = input('Masukkan menu yang dipilih: ')
+
+        is_done = select_menu(menu, id_user)
+
+        if is_done:
+            break
